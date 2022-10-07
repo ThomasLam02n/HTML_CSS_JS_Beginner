f488ecad2d424c369770790c207c3b7efb98a7e7
lamvuthanhtai02 <lamvuthanhtai14042002@gmail.com>
2022-10-07 10:06:54 +0700

    base login web cuoiki

---
+++ CuoikiWEB/assets/code php/header.php
@@ -0,0 +1,14 @@
+<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Travel</title>
+    <link rel="stylesheet" href="./assets/Bootstraps/bootstrap.css">
+    <link rel="stylesheet" href="./assets/CSS/animate.css">
+    <link rel="stylesheet" href="./assets/fronts/fontawesome-free-6.2.0-web/css/all.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css"
+    integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w=="
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="./assets/js/wow.js">
+    <link rel="stylesheet" href="./assets/CSS/animate.css">
+    <link rel="stylesheet" href="./assets/CSS/base.css">
+    <link rel="stylesheet" href="./assets/CSS/main.css">
