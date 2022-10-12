3f04cc1e53cca2df4ce9dbb5ea7f0969d56feac7
lamvuthanhtai02 <lamvuthanhtai14042002@gmail.com>
2022-10-12 10:04:12 +0700

    kiemtraweb version 2

--- kiemtraweb/assets/js/slider.js
+++ kiemtraweb/assets/js/slider.js
@@ -7,4 +7,32 @@ $(document).ready(function(){
   });
 });
 
-  
+
+$(document).ready(function(){
+  $('.project__slider').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    dots: true,
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+    ]
+  });
+});
