b71561638eb6ec1e3cf1043c106a1c4077a071a0
lamvuthanhtai02 <lamvuthanhtai14042002@gmail.com>
2022-10-17 10:28:20 +0700

    responsive calendar

--- CuoikiWEB/assets/js/slider.js
+++ CuoikiWEB/assets/js/slider.js
@@ -37,6 +37,7 @@ $(document).ready(function(){
       slidesToScroll: 1,
       autoplay: true,
       autoplaySpeed: 1000,
+      dots: true,
       responsive: [
         {
           breakpoint: 1024,
