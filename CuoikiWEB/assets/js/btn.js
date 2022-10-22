0fa481c1febe26ec402d4042c6f1c4c2334e78f2
lamvuthanhtai02 <lamvuthanhtai14042002@gmail.com>
2022-10-22 23:08:41 +0700

    fix bug font end

--- CuoikiWEB/assets/js/btn.js
+++ CuoikiWEB/assets/js/btn.js
@@ -14,4 +14,4 @@ function scrollFunction() {
 function topFunction() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
-  }
+}
---
+++ CuoikiWEB/assets/js/login.js
@@ -0,0 +1,9 @@
+function login() {
+    document.getElementById('signin').style.display='block';
+    document.getElementById('signup').style.display='none';
+}
+
+function registed() {
+    document.getElementById('signup').style.display='block';
+    document.getElementById('signin').style.display='none';
+}
