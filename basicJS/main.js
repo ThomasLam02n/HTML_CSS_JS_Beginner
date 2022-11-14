2de4efccba545e739ba73bdaccc1ed124df52e51
lamvuthanhtai02 <lamvuthanhtai14042002@gmail.com>
2022-11-14 22:34:00 +0700

    work with string JS

--- basicJS/main.js
+++ basicJS/main.js
@@ -1,16 +1,14 @@
+var myString = 'Tai Lam';
 
-var result = NaN || undefined || false;
-// console.log(result);
+console.log(myString[0]);
+console.log(myString[1]);
+console.log(myString[2]);
+console.log(myString[3]);
+console.log(myString[4]);
+console.log(myString[5]);
+console.log(myString[6]);
 
-//Bước 1: Tất cả các giá trị của result không nằm trong bản 6 giá trị false
-//Bước 2: Nó sẽ dò từng giá trị, nếu giá trị đầu tiên ko nằm trong bảng 6
-//        giá trị false thì sẽ dò tiếp
-//Bước 3: Cuối cùng nó sẽ chọn giá trị mà nằm trong bản 6 giá trị false để
-//        gán vào result và kết thúc dò - Trường hợp nếu không có bất kỳ giá trị
-//        nào false thì nó sẽ gán giá trị cuối cùng vào result 
 
-if (result) {
-    console.log('DIEU KIEN DUNG');
-}else{
-    console.log('DIEU KIEN SAI');
-}
+
+
+
