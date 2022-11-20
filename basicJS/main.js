1e48363a70f630796bc58cf3cd3f4c0d709e5b3a
lamvuthanhtai02 <lamvuthanhtai14042002@gmail.com>
2022-11-20 16:37:53 +0700

    object in JS

--- basicJS/main.js
+++ basicJS/main.js
@@ -1,7 +1,19 @@
-fullName();
-
-var fullName = function showName() {
-    console.log('Lâm Vũ Thành Tài');
+function User(firstName, lastName, avatar) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.avatar = avatar;
 }
+
+User.prototype.email = 'GDU@gmail.com'
+User.prototype.getName = function() {
+    return `${this.firstName} ${this.lastName}`;
+}
+
+var author = new User('Tai', 'Lam', 'Avatar LVTT');
+var user = new User('Phong', 'Truong', 'Avatar THP');
+
+console.log(author.getName());
+console.log(user.getName());
+
 
 
