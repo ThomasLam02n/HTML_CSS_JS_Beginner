fc39721a9eff8680896d6587170e6a0db9301bda
lamvuthanhtai02 <lamvuthanhtai14042002@gmail.com>
2022-11-21 21:29:01 +0700

    for loop JS

--- basicJS/main.js
+++ basicJS/main.js
@@ -1,16 +1,4 @@
-var date = 9;
-
-switch (date) {
-    case 2:
-    case 3:
-    case 4:
-        console.log('Hôm nay là thứ 2, 3, 4');
-        break;
-    case 5:
-        console.log('Hôm nay là thứ 5');
-        break;
-    default:
-        console.log('Không biết');
+for(var i = 1; i <= 1000; i++) {
+    console.log(i)
 }
-
 
