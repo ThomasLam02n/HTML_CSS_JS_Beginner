cf22330160659e867a1999ca86fe62ce3adcafba
lamvuthanhtai02 <lamvuthanhtai14042002@gmail.com>
2022-11-25 22:50:13 +0700

    Array methods in JS

--- basicJS/main.js
+++ basicJS/main.js
@@ -1,4 +1,30 @@
-for(var i = 100; i >= 0; i -= 5) {
-    console.log(i)
-}
+var courses = [
+    {
+        id: 1,
+        name: 'Javascript',
+        coin: 255
+    },
+    {
+        id: 2,
+        name: 'CSS',
+        coin: 0
+    },
+    {
+        id: 3,
+        name: 'HTML',
+        coin: 0
+    },
+    {
+        id: 4,
+        name: 'PHP',
+        coin: 255
+    }
+]
+
+
+var coinOfCoure = courses.filter(function(course, index){
+    return course.coin === 200;
+});
+
+console.log(coinOfCoure);
 
