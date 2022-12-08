6e304f5335b9493d87c47888e78be3e52257479a
lamvuthanhtai02 <lamvuthanhtai14042002@gmail.com>
2022-12-08 23:52:36 +0700

    intro callback JS

--- basicJS/main.js
+++ basicJS/main.js
@@ -1,3 +1,19 @@
-var maxNumber = Math.max(10, 23, 46, 99, 3);
-console.log(maxNumber);
+Array.prototype.find2 = function(callBack){
+    
+}
 
+var courses = [
+    'javascript',
+    'PHP',
+    'Ruby'
+];
+
+var htmls = courses.find2(function(course){
+    return course.includes('PHP');
+});
+
+// var myCourse = courses.find(function(course, index){
+//     return course.includes('PHP');
+// });
+
+// console.log(myCourse);
