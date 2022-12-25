155ae5683630c9a516461887461677483e719b58
lamvuthanhtai02 <lamvuthanhtai14042002@gmail.com>
2022-12-25 18:58:06 +0700

    recursive in JS

--- basicJS/main.js
+++ basicJS/main.js
@@ -1,40 +1,11 @@
-Array.prototype.every2 = function(callBack) {
-    var output = true;
-    for (var index in this) {
-        if(this.hasOwnProperty(index)) {
-            var result = callBack(this[index], index, this);
-            if(!result) {
-                output = false;
-                break;
-            }
-        }
-    }
+// 5 * 4 * 3 * 2 * 1 = 120
 
-    return output;
+function giaiThua(number) {
+    if (number > 0) {
+        return number * giaiThua(number - 1)
+    }
+    return 1;
 };
 
-var courses = [
-    {
-        name: 'Javascript',
-        coin: 680,
-        isFinish: true
-    },
-    {
-        name: 'PHP',
-        coin: 860,
-        isFinish: true
-    },
-    {
-        name: 'Javascript',
-        coin: 980,
-        isFinish: true
-    }
-];
-
-
-var result = courses.every2(function(courses, index, array){
-    return courses.isFinish;
-});
-
-console.log(result);
+console.log(giaiThua(5));
 
