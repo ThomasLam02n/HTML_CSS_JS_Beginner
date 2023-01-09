c0b195b239da580be54f45336f4f89cc4a3da515
lamvuthanhtai02 <lamvuthanhtai14042002@gmail.com>
2023-01-09 20:54:21 +0700

    DOM documnet

--- basicJS/main.js
+++ basicJS/main.js
@@ -1,11 +1,1 @@
-// 5 * 4 * 3 * 2 * 1 = 120
-
-function giaiThua(number) {
-    if (number > 0) {
-        return number * giaiThua(number - 1)
-    }
-    return 1;
-};
-
-console.log(giaiThua(5));
-
+document.write('Lâm Vũ Thành Tài');
