e09a05b2c015c0bea299ce359db29061b1077f52
lamvuthanhtai02 <lamvuthanhtai14042002@gmail.com>
2023-02-09 10:10:42 +0700

    preventDefault and stopPropagation

--- basicJS/main.js
+++ basicJS/main.js
@@ -1,5 +1,11 @@
-var inputElement = document.querySelector('input[type="text"]');
-inputElement.onkeyup = function(e){
-    console.log(e.which);
-}
+document.querySelector('div').onclick =
+  function(e) {
+    console.log('Thẻ DIV');
+  };
+
+  document.querySelector('button').onclick =
+  function(e) {
+    e.stopPropagation();
+    console.log('Thẻ button');
+  };
 
